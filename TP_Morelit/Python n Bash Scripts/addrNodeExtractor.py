58f4cdd4ac5643da5581b77d76363d10885089de
Rafagan Soares de Carvalho Sampaio Mariz <rafagancarvalho@gmail.com>
2017-07-02 15:03:14 -0300

    Scripts python
    Scripts para pegar os endereços e pegar os valores em csv para ajudar interface em Java, os arquivos em bsh estao na pasta do grupo no pc no lab, irei coloca-los ao completar e testar

---
+++ TP_Morelit/Python n Bash Scripts/addrNodeExtractor.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+# -*- coding: iso-8859-15 -*-
+
+import os, sys
+
+fileNodes = open("listNodes.txt", "r+")
+fileAddr = open("addrList.txt", "w+")
+nodesReq = fileNodes.read(); #Le todos os dados para separar os enderecos dos nos visiveis
+
+#print nodesReq.split("|-", 8)
+
+for line in nodesReq.split("|-", 8):
+	#print line
+	if line.startswith('0013'): # verifica se a linha e comeca com o endereço
+		#print line[0:15]
+		fileAddr.write(line[0:15] " ")
+
+fileNodes.close()
+fileAddr.close()
