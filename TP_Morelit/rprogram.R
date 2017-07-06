f31fab4b26eb4299a7d2e88dfebeab003ae8c4b3
kethorinironfists <reis.nich@gmail.com>
2017-07-06 09:58:19 -0300

    mudanças .r

--- TP_Morelit/rprogram.R
+++ TP_Morelit/rprogram.R
@@ -1,2 +1,6 @@
+rm(list=ls(all=TRUE))
+setwd("/Users/nicholas/Desktop/IoT/TP_Morelit")
 data = read.csv('exportedData.csv', sep=',', stringsAsFactors = FALSE)
+data
+jpeg("rplot.jpg")
 plot(as.numeric(data[data[,2] == 1, 5]))
